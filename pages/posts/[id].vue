3d730b8d2f6e94e0c1fdf606dc96e74cdee744c3
ltroya-as <luis.troya@allansolutions.com>
2022-11-26 13:01:05 -0500

    feat: add swr

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -3,14 +3,16 @@ const route = useRoute();
 const { data: post } = await useFetch(
   `https://jsonplaceholder.typicode.com/posts/${route.params.id}`
 );
-const { data: response } = await useFetch("/api/date");
+const { data: response } = await useFetch(
+  "https://worldtimeapi.org/api/timezone/America/Bogota"
+);
 
 useHead({ title: post.title });
 </script>
 
 <template>
   <div>
-    <p>Current date: {{ response.currentDate }}</p>
+    <p>Current date: {{ response.utc_datetime }}</p>
     <h1>Post title: {{ post.title }}</h1>
     <p>Post body:</p>
     <p>{{ post.body }}</p>
